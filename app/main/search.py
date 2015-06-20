2d955ed82aaed8aea1021ad6dfe35e7fbbcdfc9c
Shinya Fujino <shf0811@gmail.com>
2015-06-20 21:26:52 +0900

    Add SearchEngine class and implement advanced search

---
+++ app/main/search.py
@@ -0,0 +1,159 @@
+from elasticsearch import Elasticsearch
+
+
+class SearchEngine:
+
+    index = 'aozora-search'
+
+    def __init__(self):
+        self.es = Elasticsearch()
+
+    def search(self, q, page):
+        query = {
+            'match': {
+                'main_text': {
+                    'query': q,
+                    'operator': 'and'
+                }
+            }
+        }
+        highlight = {
+            'fields': {
+                'main_text': {}
+            }
+        }
+        from_ = (page - 1) * 10
+
+        res = self.es.search(
+            index=SearchEngine.index,
+            body={
+                'query': query,
+                'highlight': highlight,
+                'from': from_
+            }
+        )
+        total = res['hits']['total']
+
+        return res, total
+
+    def advanced_search(self, q, author, page):
+        must = []
+        if q:
+            query = {
+                'match': {
+                    'main_text': {
+                        'query': q,
+                        'operator': 'and'
+                    }
+                }
+            }
+        if author:
+            must.append({'term': {'author': author}})
+
+        highlight = {
+            'fields': {
+                'main_text': {}
+            }
+        }
+        from_ = (page - 1) * 10
+
+        if q and must:
+            res = self.es.search(
+                index=SearchEngine.index,
+                body={
+                    'query': {
+                        'filtered': {
+                            'query': query,
+                            'filter': {
+                                'bool': {
+                                    'must': must
+                                }
+                            }
+                        }
+                    },
+                    'highlight': highlight,
+                    'from': from_
+                }
+            )
+        elif not must:
+            res = self.es.search(
+                index=SearchEngine.index,
+                body={
+                    'query': query,
+                    'highlight': highlight,
+                    'from': from_
+                }
+            )
+        elif not q:
+            res = self.es.search(
+                index=SearchEngine.index,
+                body={
+                    'query': {
+                        'filtered': {
+                            'filter': {
+                                'bool': {
+                                    'must': must
+                                }
+                            }
+                        }
+                    },
+                    'from': from_
+                }
+            )
+            for r in res['hits']['hits']:
+                r['highlight'] = None
+        else:
+            pass  # err
+        total = res['hits']['total']
+
+        return res, total
+
+    def search_book(self, book_id):
+        query = {
+            'filtered': {
+                'query': {
+                    'match_all': {}
+                },
+                'filter': {
+                    'term': {
+                        'book_id': book_id
+                    }
+                }
+            }
+        }
+
+        return self.es.search(
+            index=SearchEngine.index,
+            body={
+                'query': query
+            }
+        )
+
+    def search_inside_book(self, q, book_id):
+        query = {
+            "filtered": {
+                "query": {
+                    "match": {
+                        "main_text": q
+                    }
+                },
+                "filter": {
+                    "term": {
+                        "book_id": book_id
+                    }
+                }
+            }
+        }
+        highlight = {
+            'fields': {
+                'main_text': {}
+            }
+        }
+
+        return self.es.search(
+            index=SearchEngine.index,
+            body={
+                'query': query,
+                'highlight': highlight
+            }
+        )
--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,9 @@ from flask import abort, render_template, redirect, request, url_for
 from . import main
 from .forms import SearchForm, AdvancedSearchForm
 
-from elasticsearch import Elasticsearch
+from .search import SearchEngine
 
-es = Elasticsearch()
+engine = SearchEngine()
 
 
 @main.route('/', methods=['GET'])
@@ -13,69 +13,54 @@ def index():
     return render_template('index.html', form=form)
 
 
-@main.route('/search', methods=['GET', 'POST'])
+@main.route('/search', methods=['GET'])
 def search():
-    if request.method == 'GET':
-        form = SearchForm()
-        q = request.args.get('q')
-        query = {'match': {'main_text': {'query': q, 'operator': 'and'}}}
-        highlight = {'fields': {'main_text': {}}}
-        if request.args.get('page') and request.args.get('page').isdigit():
-            page = int(request.args.get('page'))
-            from_ = (page - 1) * 10
-        else:
-            page = 1
-            from_ = 0
-        res = es.search(index='aozora-search', body={'query': query, 'highlight': highlight, 'from': from_})
-        total = res['hits']['total']
-        return render_template('search.html', res=res, page=page, pagenate=range(1, (total - 1) // 10 + 2), q=q)
+    form = SearchForm()
+    q = request.args.get('q')
+    if not q:
+        redirect('/')  # not work
+    if request.args.get('page') and request.args.get('page').isdigit():
+        page = int(request.args.get('page'))
     else:
-        return 'not yet'
+        page = 1
+
+    res, total = engine.search(q, page)
+    query_str = 'q={}'.format(q)
+    return render_template('search.html', res=res, query_str=query_str, page=page, pagenate=range(1, (total - 1) // 10 + 2), q=q)
 
 
 @main.route('/advanced-search', methods=['GET'])
 def advanced_search():
-    form = AdvancedSearchForm()
-    print(dir(form))
-    return render_template('advanced_search.html', form=form)
+    form = AdvancedSearchForm(action='/advanced-search')
+
+    if len(request.args):
+        q = request.args.get('q')
+        author = request.args.get('author')
+        if request.args.get('page') and request.args.get('page').isdigit():
+            page = int(request.args.get('page'))
+        else:
+            page = 1
+        if not any([q, author]):
+            return render_template('advanced_search_top.html', form=form)
+
+        res, total = engine.advanced_search(q, author, page)
+        query_str = 'q={}&author={}'.format(q, author)
+        return render_template('search.html', res=res, query_str=query_str, page=page, pagenate=range(1, (total - 1) // 10 + 2), q=q)
+
+    return render_template('advanced_search_top.html', form=form)
 
 
 @main.route('/book/<int:book_id>', methods=['GET'])
 def book(book_id):
-    query = {
-        'filtered': {
-            'query': {
-                'match_all': {}
-            },
-            'filter': {
-                'term': {
-                    'book_id': book_id
-                }
-            }
-        }
-    }
-    res = es.search(index='aozora-search', body={'query': query})
+    res = engine.search_book(book_id)
     if res['hits']['total']:
         q = request.args.get('q')
-        if q is None:
-            abort(404)
-        query = {
-            "filtered": {
-                "query": {
-                    "match": {
-                        "main_text": q
-                    }
-                },
-                "filter": {
-                    "term": {
-                        "book_id": book_id
-                    }
-                }
-            }
-        }
-        highlight = {'fields': {'main_text': {}}}
-        hits = es.search(index='aozora-search', body={'query': query, 'highlight': highlight})
-        return render_template('book.html', res=res['hits']['hits'][0]['_source'], hits=hits['hits']['hits'][0]['highlight']['main_text'])
+        if q:
+            hits = engine.search_inside_book(q, book_id)
+            hits = hits['hits']['hits'][0]['highlight']['main_text']
+        else:
+            hits = None
+        return render_template('book.html', res=res['hits']['hits'][0]['_source'], hits=hits)
     else:
         abort(404)
 
